3ef9584eb25831cb711b54a8bc7693ab4b82b3e3
johnny <>
2021-03-11 17:32:09 +0800

    update

--- MovieApp/Controls/Rating.swift
+++ MovieApp/Controls/Rating.swift
@@ -2,7 +2,40 @@
 //  Rating.swift
 //  MovieApp
 //
-//  Created by 梁鑫文 on 2021/3/10.
+//  Created by johnny on 2021/3/11.
 //
 
-import Foundation
+import SwiftUI
+
+struct Rating: View {
+    
+    // @Binding 雙向綁定
+    @Binding var rating:Int?
+    
+    private func starType(index:Int)->String{
+        // if let 解包
+        if let rating = self.rating{
+            return index < rating ? "star.fill":"star"
+        }else{
+            return "star"
+        }
+    }
+    
+    var body: some View {
+        HStack{
+            ForEach(1...10,id:\.self){ index in
+                Image(systemName: self.starType(index: index))
+                    .foregroundColor(.orange)
+                    .onTapGesture {
+                        self.rating = index;
+                    }
+            }
+        }
+    }
+}
+
+struct Rating_Previews: PreviewProvider {
+    static var previews: some View {
+        Rating(rating: .constant(3))
+    }
+}
--- MovieApp/Loaders/ImageLoader.swift
+++ MovieApp/Loaders/ImageLoader.swift
@@ -10,4 +10,27 @@ import Foundation
 // download image
 class ImageLoader: ObservableObject {
     
+    // published 修飾的屬性,當這個 Property 被更新時，會發送 (emit) 一個事件通知給監控它的物件。
+    @Published var downloadedDate:Data?
+    
+    func downloadImage(url:String){
+        
+        // guard 也是像 if else 的判斷
+        guard let imageUrl = URL(string: url) else {
+            return
+        }
+        
+        URLSession.shared.dataTask(with: imageUrl) { (data, uRLResponse, error) in
+            
+            guard let data = data, error == nil else{
+                return
+            }
+            
+            DispatchQueue.main.async {
+                self.downloadedDate = data
+            }
+            
+        }.resume()
+    }
+    
 }
